ed37ff7e27310c8b5b5c9b6efcae1294fa98ca95
Nico44YT <Nico_44-YouTube@gmx.de>
2026-09-06 15:25:51 +0200

    Added name to LoadedBloodTypeContainer

--- src/main/java/nico/bloodframework/data/LoadedBloodTypeContainer.java
+++ src/main/java/nico/bloodframework/data/LoadedBloodTypeContainer.java
@@ -2,16 +2,22 @@ package nico.bloodframework.data;
 
 import com.google.gson.JsonObject;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 
-public record LoadedBloodTypeContainer(Identifier identifier, Identifier texture, int color) {
+public record LoadedBloodTypeContainer(String translationKey, Identifier identifier, Identifier texture, int color) {
+
+    public Text getName() {
+        return Text.translatable(translationKey());
+    }
 
     public static LoadedBloodTypeContainer fromJson(Identifier identifier, JsonObject json) {
+        String translationKey = JsonHelper.getString(json, "name", identifier.toTranslationKey("blood_type", "name"));
         Identifier texture = new Identifier(json.get("texture").getAsString());
         int color = JsonHelper.getInt(json, "color", 0xFF_FF_FF_FF);
 
-        return new LoadedBloodTypeContainer(identifier, texture, color);
+        return new LoadedBloodTypeContainer(translationKey, identifier, texture, color);
     }
 
     public void write(PacketByteBuf packetByteBuf) {
@@ -21,6 +27,11 @@ public record LoadedBloodTypeContainer(Identifier identifier, Identifier texture
     }
 
     public static LoadedBloodTypeContainer fromByteBuf(PacketByteBuf packetByteBuf) {
-        return new LoadedBloodTypeContainer(packetByteBuf.readIdentifier(), packetByteBuf.readIdentifier(), packetByteBuf.readInt());
+        return new LoadedBloodTypeContainer(
+                packetByteBuf.readString(),
+                packetByteBuf.readIdentifier(),
+                packetByteBuf.readIdentifier(),
+                packetByteBuf.readInt()
+        );
     }
 }
