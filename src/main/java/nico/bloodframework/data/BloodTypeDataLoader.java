332efad75051b539b950495db9480fe1631e168a
Nico44YT <Nico_44-YouTube@gmx.de>
2026-09-07 01:13:10 +0200

    Added texture tint field, jsonc support and hex number support

--- src/main/java/nico/bloodframework/data/BloodTypeDataLoader.java
+++ src/main/java/nico/bloodframework/data/BloodTypeDataLoader.java
@@ -2,6 +2,7 @@ package nico.bloodframework.data;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.resource.Resource;
@@ -10,13 +11,14 @@ import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import nico.bloodframework.Bloodframework;
 
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BloodTypeDataLoader implements SimpleSynchronousResourceReloadListener {
     private static BloodTypeDataLoader instance;
-    private static final Gson GSON = new Gson();
 
     // Looks for data/<namespace>/blood_type/*.json
     private static final String DIRECTORY = "blood_type";
@@ -44,24 +46,23 @@ public class BloodTypeDataLoader implements SimpleSynchronousResourceReloadListe
 
         Map<Identifier, Resource> resources = manager.findResources(
                 DIRECTORY,
-                id -> id.getPath().endsWith(".json")
+                BloodTypeDataLoader::allowedFile
         );
 
         for (Map.Entry<Identifier, Resource> entry : resources.entrySet()) {
             Identifier id = entry.getKey();
 
-            try (InputStreamReader reader =
-                         new InputStreamReader(entry.getValue().getInputStream())) {
+            try (BufferedReader reader =
+                         new BufferedReader(new InputStreamReader(entry.getValue().getInputStream()))) {
 
-                JsonObject json = GSON.fromJson(reader, JsonObject.class);
+                String fullJson = cleanFromComments(reader.lines().collect(Collectors.joining("\n")));
+                JsonObject json = JsonParser.parseString(fullJson).getAsJsonObject();
 
-                // Remove ".json" from the path
-                String path = id.getPath()
-                        .substring(DIRECTORY.length() + 1)
-                        .replaceAll("\\.json$", "");
+                // Remove file extension from the path
+                String path = id.getPath().substring(DIRECTORY.length() + 1, id.getPath().length() - getFileExtension(id).length() - 1);
 
-                String key = id.getNamespace() + ":" + path;
-                data.put(Identifier.tryParse(key), LoadedBloodTypeContainer.fromJson(Identifier.tryParse(key), json));
+                Identifier key = Identifier.of(id.getNamespace(), path);
+                data.put(key, LoadedBloodTypeContainer.fromJson(key, json));
 
             } catch (Exception e) {
                 System.err.println("Failed to load " + id);
@@ -72,5 +73,20 @@ public class BloodTypeDataLoader implements SimpleSynchronousResourceReloadListe
 
     public static Map<Identifier, LoadedBloodTypeContainer> getData() {
         return instance.data;
+    }
+
+    // https://stackoverflow.com/questions/8626866/java-removing-comments-from-string
+    private static String cleanFromComments(String string) {
+        return string.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
+    }
+
+    private static boolean allowedFile(Identifier fileId) {
+        return fileId.getPath().endsWith(".jsonc") ||
+                fileId.getPath().endsWith(".json");
+    }
+
+    private static String getFileExtension(Identifier fileId) {
+        String[] parts = fileId.getPath().split("\\.");
+        return parts[parts.length - 1];
     }
 }
--- src/main/java/nico/bloodframework/data/LoadedBloodTypeContainer.java
+++ src/main/java/nico/bloodframework/data/LoadedBloodTypeContainer.java
@@ -1,38 +1,107 @@
 package nico.bloodframework.data;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 
-public record LoadedBloodTypeContainer(String translationKey, Identifier identifier, Identifier texture, int color) {
+public record LoadedBloodTypeContainer(
+        Identifier identifier,
+        // Properties
+        String translationKey,
+        Identifier texture,
+        int textureTint,
+        int color
+) {
+    private static final String KEY_NAME = "name";
+    private static final String KEY_TEXTURE = "texture";
+    private static final String KEY_FILE = "file";
+    private static final String KEY_TINT = "tint";
+    private static final String KEY_COLOR = "color";
 
     public Text getName() {
         return Text.translatable(translationKey());
     }
 
     public static LoadedBloodTypeContainer fromJson(Identifier identifier, JsonObject json) {
-        String translationKey = JsonHelper.getString(json, "name", identifier.toTranslationKey("blood_type", "name"));
-        Identifier texture = new Identifier(json.get("texture").getAsString());
-        int color = JsonHelper.getInt(json, "color", 0xFF_FF_FF_FF);
+        String translationKey = JsonHelper.getString(
+                json,
+                KEY_NAME,
+                identifier.toTranslationKey("blood_type", "name")
+        );
 
-        return new LoadedBloodTypeContainer(translationKey, identifier, texture, color);
+        Identifier texture;
+        int textureTint;
+        JsonElement textureElement = json.get(KEY_TEXTURE);
+
+        if (textureElement.isJsonPrimitive()) {
+            texture = new Identifier(textureElement.getAsString());
+            textureTint = 0xFF_FF_FF_FF;
+
+        } else if (textureElement.isJsonObject()) {
+            JsonObject textureObject = textureElement.getAsJsonObject();
+            if (textureObject.has(KEY_TEXTURE)) {
+                texture = new Identifier(textureObject.get(KEY_TEXTURE).getAsString());
+            } else {
+                texture = new Identifier(textureObject.get(KEY_FILE).getAsString());
+            }
+            textureTint = getIntFromJsonElement(textureObject.get(KEY_TINT), 0xFF_FF_FF_FF);
+
+        } else {
+            throw new RuntimeException(String.format(
+                    "Texture element of %s is not an object nor a primitive!",
+                    identifier
+            ));
+        }
+
+        int color = getIntFromJsonElement(json.get(KEY_COLOR), 0xFF_FF_FF_FF);
+
+        return new LoadedBloodTypeContainer(
+                identifier,
+                translationKey,
+                texture,
+                textureTint,
+                color
+        );
     }
 
     public void write(PacketByteBuf packetByteBuf) {
-        packetByteBuf.writeString(translationKey);
-        packetByteBuf.writeIdentifier(identifier);
-        packetByteBuf.writeIdentifier(texture);
-        packetByteBuf.writeInt(color);
+        packetByteBuf.writeIdentifier(identifier); // Identifier
+        packetByteBuf.writeString(translationKey); // Translation key
+        packetByteBuf.writeIdentifier(texture); // Texture Identifier
+        packetByteBuf.writeInt(textureTint); // Texture Tint
+        packetByteBuf.writeInt(color); // Color
     }
 
     public static LoadedBloodTypeContainer fromByteBuf(PacketByteBuf packetByteBuf) {
         return new LoadedBloodTypeContainer(
-                packetByteBuf.readString(),
-                packetByteBuf.readIdentifier(),
-                packetByteBuf.readIdentifier(),
-                packetByteBuf.readInt()
+                packetByteBuf.readIdentifier(), // Identifier
+                packetByteBuf.readString(), // Translation key
+                packetByteBuf.readIdentifier(), // Texture Identifier
+                packetByteBuf.readInt(), // Texture Tint
+                packetByteBuf.readInt() // Color
         );
+    }
+
+    private static int getIntFromJsonElement(JsonElement jsonElement, int defaultValue) {
+        if (jsonElement.isJsonPrimitive()) return getIntFromJsonElement(jsonElement.getAsJsonPrimitive(), defaultValue);
+        throw new RuntimeException(jsonElement + " can only be a number or a string");
+    }
+
+    private static int getIntFromJsonElement(JsonPrimitive jsonPrimitive, int defaultValue) {
+        if (jsonPrimitive == null) return defaultValue;
+
+        if (jsonPrimitive.isNumber()) {
+            return jsonPrimitive.getAsInt();
+        }
+
+        if (jsonPrimitive.isString()) {
+            return Integer.decode(jsonPrimitive.getAsString().replace("_", ""));
+        }
+
+        throw new RuntimeException(jsonPrimitive + " can only be a number or a string");
     }
 }
